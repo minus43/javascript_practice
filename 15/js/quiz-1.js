8c8b5c44b7a65d481443625102ac25694e1532b3
minus43 <minu425854@gmail.com>
2025-03-13 19:00:43 +0900

    챕터 15 완료

---
+++ 15/js/quiz-1.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.font ='bold 300px sans-serif';
+ctx.fillText('GOOD', 100, 320);
--- 15/js/quiz-2.js
+++ 15/js/quiz-2.js
@@ -4,3 +4,11 @@ const ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.font = "bold 300px sans-serif";
 ctx.fillText("GOOD", 100, 320);
+
+ctx.globalCompositeOperation = "source-in";
+
+let img = new Image();
+img.src = "images/text-bg.jpg";
+img.onload = () => {
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+};
